fe64fc19d090dc0568d54e27c0321909e5aa25a8
Shane Loretz <shane.loretz@gmail.com>
2017-03-04 09:09:22 -0800

    Fixed implicit declaration error
    os_dep/linux/rtw_android.c:348:11: error: implicit declaration of function ‘strnicmp’ [-Werror=implicit-function-declaration]

--- os_dep/linux/rtw_android.c
+++ os_dep/linux/rtw_android.c
@@ -38,6 +38,13 @@
 #include <linux/irq.h>
 #endif
 
+#include <linux/string.h>
+// Fancy stack overflow answer
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 0, 0))
+#define strnicmp strncasecmp
+#endif
+
+
 extern void macstr2num(u8 *dst, u8 *src);
 
 const char *android_wifi_cmd_str[ANDROID_WIFI_CMD_MAX] = {
